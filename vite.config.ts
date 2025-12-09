d3b04bbb8b15ef95a4bd8059061ec602a78cf2c2
Mateus Seger Selau <mateus.selau@herval.com.br>
2025-12-09 13:53:52 -0300

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
         alias: {
             '@': path.resolve(__dirname, './src'),
         },
-        dedupe: ['react', 'react-dom'],
+        dedupe: ['react', 'react-dom', 'react-router-dom'],
     },
 })
